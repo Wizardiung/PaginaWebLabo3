56352c45ae6f9542a9c00104df8555e117126882
Wizardiung <santiicarignano@gmail.com>
2024-10-28 00:45:27 -0300

    Primer Carga
    Subida de todos los archivos del momento del sitio web

---
+++ script.js
@@ -0,0 +1,19 @@
+// Obtener el botón
+const btnScrollTop = document.getElementById("btnScrollTop");
+
+// Mostrar el botón cuando el usuario ha desplazado hacia abajo 100px
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        btnScrollTop.style.display = "block";
+    } else {
+        btnScrollTop.style.display = "none";
+    }
+};
+
+// Cuando el usuario hace clic en el botón, desplazarse hacia arriba suavemente
+btnScrollTop.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Desplazamiento suave
+    });
+};
